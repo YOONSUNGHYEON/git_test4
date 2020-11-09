ab0b63a192a1b07559186a3ae5e2499189fa1d0e
YOONSUNGHYEON <qead753951@naver.com>
2020-11-09 22:47:09 +0900

    quick_sort 구현

--- src/git_test4/Sort.java
+++ src/git_test4/Sort.java
@@ -2,7 +2,14 @@ package git_test4;
 
 public class Sort {
 
-	static int partition(int[] a, int start, int end) { // TODO: partition 알고리즘을 구현해야 함
+	static int partition(int[] a, int start, int end) {
+		int value = a[end];
+		int i = start - 1;
+		for (int j = start; j <= end - 1; ++j)
+			if (a[j] < value)
+				swap(a, ++i, j);
+		swap(a, i + 1, end);
+		return i + 1;
 	}
 
 	static void quickSort(int[] a, int start, int end) {
@@ -22,4 +29,5 @@ public class Sort {
 		a[i] = a[j];
 		a[j] = temp;
 	}
+
 }
